98bf92c08567a7df220a46c546e8dc4505fd8457
Haritha <haritha_kc@DellTeam.com>
2023-12-17 11:20:05 +0530

    Test class added for controller class

--- src/main/java/org/src/AdequateShopApplication.java
+++ src/main/java/org/src/AdequateShopApplication.java
@@ -7,10 +7,8 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling // <-- added
 @OpenAPIDefinition(info = @Info(title = "adequate-shop-application", description = "Adequate shop Services", version = "v1"))
 @SecurityScheme(name = "security_auth", scheme = "api_key", type = SecuritySchemeType.APIKEY, paramName = "JWT", in = SecuritySchemeIn.HEADER)
 public class AdequateShopApplication {
